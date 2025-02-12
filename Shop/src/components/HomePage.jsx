1aa7cd19d12ebda634b99fc15c8c557e4510eb3c
kevin <kmeynard.1@gmail.com>
2025-02-12 21:34:18 +0900

    feat : shop page grid / items

--- Shop/src/components/HomePage.jsx
+++ Shop/src/components/HomePage.jsx
@@ -1,7 +1,48 @@
 import NavBar from './NavBar'
 import './HomePage.css'
+import { useEffect, useState } from 'react'
 
 function HomePage (){
+    const url = 'https://fakestoreapi.com/products'
+    const [data, setData] = useState("")
+
+    useEffect(()=>{
+        fetch(`${url}`)
+            .then(res=>res.json())
+            .then(json=>{
+                setData(json)
+            })
+
+    },[])
+    
+    
+
+    function Item(data){
+        if (!data){
+            return(
+                <p>Loading</p>
+            )
+        }
+        return(
+            <div className="Items">
+                <img src={data['image']} alt=""  width={100}/>
+                <div className='Description'>
+                    <p>{data['title']}</p>
+                    <p>{data['price']}</p>
+                </div>
+                <button className="Add" />
+            </div>
+        )
+    }
+    
+    function EveryItems(data){
+        let listItem = []
+        for(let i of data){
+            listItem.push(Item(i))
+        }
+        return listItem
+    }
+
     return(
         <div>
             <div className='Header'>
@@ -13,6 +54,11 @@ function HomePage (){
                     <img src="" alt="$" />
                 </div>
             </div>
+           <div className='DisplayItems'>
+            {
+                EveryItems(data)
+            }
+           </div>
         </div>
     )
 }
